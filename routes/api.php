afe15bede51ba93423412a9e2218f53594d77e64
bidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2025-06-27 18:10:56 +0530

    update no cache middleware

--- routes/api.php
+++ routes/api.php
@@ -70,7 +70,6 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::post('/store/custom-booking', 'App\Http\Controllers\Api\PackageController@storeMultipleOrders');
     Route::get('/edit-custom-package', 'App\Http\Controllers\Api\PackageController@editCustomPackage');
     Route::get('/package-booking-lists', 'App\Http\Controllers\Api\PackageController@getBookingLists');
-    Route::get('/package-show-lists', 'App\Http\Controllers\Api\PackageController@showPackage');
     Route::get('/cities', function (Request $request) {
         $input = $request->query('input');
         $response = Http::get("https://maps.googleapis.com/maps/api/place/autocomplete/json", [
