a3689a7e7b32a87142f335abae9aa83d593c728d
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2025-07-09 11:38:06 +0530

    packaged attraction booking

--- routes/web.php
+++ routes/web.php
@@ -467,6 +467,8 @@ Route::group(['middleware' => ['auth', 'no.cache']], function () {
         Route::post('/zones/{zone}/settings', [ZoneController::class, 'saveSettings'])->name('zones.settings');
         // Route::post('/cities/store', [PortController::class, 'store'])->name('cities.store');
 
+        // Restaurant Coupon
+        Route::post('/generate-restaurant-coupon', [RestaurantController::class, 'generateCoupon'])->name('generate.restaurant.coupon');
         
     });
 
