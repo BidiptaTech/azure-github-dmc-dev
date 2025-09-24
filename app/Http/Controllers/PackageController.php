a9ec12aa646529dac49bf66b52b3577548977492
bidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2025-09-24 17:02:17 +0530

    update booking

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -643,8 +643,11 @@ class PackageController extends Controller
     public function predefinedPackageBookingList()
     {
         $user = Auth::user();
-
-        if($user->role_id == 11 || $user->role_id == 33 || $user->role_id == 34 || $user->role_id == 36 || $user->role_id == 37 || $user->role_id == 38 || $user->role_id == 128 || $user->role_id == 129 || $user->role_id == 130 || $user->role_id == 131 || $user->role_id == 132 || $user->role_id == 133 || $user->role_id == 134 || $user->role_id == 135 || $user->role_id == 136 || $user->role_id == 137 || $user->role_id == 138){
+        if($user->role_id == 11 || $user->role_id == 33 || $user->role_id == 34 || $user->role_id == 36 || 
+        $user->role_id == 37 || $user->role_id == 38 || $user->role_id == 128 || $user->role_id == 129 || $user->role_id == 130 || $user->role_id == 131 || $user->role_id == 132 ||
+         $user->role_id == 133 || $user->role_id == 134 || $user->role_id == 135 || $user->role_id == 136 ||
+          $user->role_id == 137 || $user->role_id == 138 || $user->role_id == 126 || $user->role_id == 124 ||
+           $user->role_id == 127 || $user->role_id == 125){
             if($user->role_id == 11 || $user->role_id == 20){
                 $dmc_id = $user->userId;
             }
