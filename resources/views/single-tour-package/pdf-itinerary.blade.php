eaf1ed9ce3f578fb8f4ff71ea007e44e8d9f76d5
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2026-01-08 13:58:45 +0530

    cretae file change

--- resources/views/single-tour-package/pdf-itinerary.blade.php
+++ resources/views/single-tour-package/pdf-itinerary.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Tour Quotation</title>
+    <title>Tour Quotation </title>
     <style>
         * {
             font-family: 'DejaVu Sans', Arial, sans-serif;
