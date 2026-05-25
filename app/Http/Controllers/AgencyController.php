c7a52f30539e385c439f9be7456f7d736abaaf97
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2026-05-25 15:25:03 +0530

    update agency field card number to license number field

--- app/Http/Controllers/AgencyController.php
+++ app/Http/Controllers/AgencyController.php
@@ -67,7 +67,7 @@ class AgencyController extends Controller
             'contact_person' => 'required|string|max:255',
             'address' => 'required|string',
             'postal_code' => 'nullable|string|max:20',
-            'id_card_type' => 'string|max:255',
+            // 'id_card_type' => 'string|max:255',
             'card_number' => 'string|max:50',
             'agency_logo' => 'required|nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'branches' => 'nullable|array',
@@ -122,7 +122,7 @@ class AgencyController extends Controller
                 'contact_person' => $request->input('contact_person'),
                 'address' => $request->input('address'),
                 'postal_code' => $request->input('postal_code'),
-                'id_card_type' => $request->input('id_card_type'),
+                // 'id_card_type' => $request->input('id_card_type'),
                 'card_number' => $request->input('card_number'),
                 'branches' => $this->normalizeBranches($request->input('branches', [])),
                 'logo' => $logoPath,
@@ -165,7 +165,7 @@ class AgencyController extends Controller
         $agency->contact_person = $request->input('contact_person');
         $agency->address = $request->input('address');
         $agency->postal_code = $request->input('postal_code');
-        $agency->id_card_type = $request->input('id_card_type');
+        // $agency->id_card_type = $request->input('id_card_type');
         $agency->card_number = $request->input('card_number');
         $agency->branches = $this->normalizeBranches($request->input('branches', []));
         $agency->logo = $logoPath;
@@ -214,7 +214,7 @@ class AgencyController extends Controller
             'contact_person' => 'required|string|max:255',
             'address' => 'required|string',
             'postal_code' => 'nullable|string|max:20',
-            'id_card_type' => 'string|max:255',
+            // 'id_card_type' => 'string|max:255',
             'card_number' => 'string|max:50',
             'agency_logo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'branches' => 'nullable|array',
@@ -254,7 +254,7 @@ class AgencyController extends Controller
         $agency->contact_person = $request->input('contact_person');
         $agency->address = $request->input('address');
         $agency->postal_code = $request->input('postal_code');
-        $agency->id_card_type = $request->input('id_card_type');
+        // $agency->id_card_type = $request->input('id_card_type');
         $agency->card_number = $request->input('card_number');
         $agency->branches = $this->normalizeBranches($request->input('branches', []));
         $agency->updated_by = Auth::user()->userId;
