d675d71fd236296151d4990664eca3aa96f6b076
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2026-06-04 16:41:45 +0530

    updated mail

--- resources/views/day-level/edit.blade.php
+++ resources/views/day-level/edit.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Edit Day Level')
 
 @section('content')
+
 <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="d-flex justify-content-between align-items-center mb-3">
