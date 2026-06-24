c0520c5954f137311abe58994db37e346a674cbd
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2026-06-24 16:13:07 +0530

    fix mail

--- app/Mail/DmcMail.php
+++ app/Mail/DmcMail.php
@@ -3,51 +3,31 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class DmcMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    /**
+     * Create a new message instance.
+     */
     public $htmlContent;
-
     public $emailSubject;
 
-    public ?string $fromEmail;
-
-    public ?string $fromName;
-
-    public ?string $replyToEmail;
-
-    public function __construct(
-        $htmlContent,
-        $subject = null,
-        ?string $fromEmail = null,
-        ?string $fromName = null,
-        ?string $replyToEmail = null
-    ) {
+    public function __construct($htmlContent, $subject = null)
+    {
         $this->htmlContent = $htmlContent;
         $this->emailSubject = $subject ?: 'Booking Confirmation';
-        $this->fromEmail = $fromEmail;
-        $this->fromName = $fromName;
-        $this->replyToEmail = $replyToEmail;
     }
 
     public function build()
     {
-        $mail = $this->subject($this->emailSubject)
-            ->html($this->htmlContent);
-
-        if ($this->fromEmail && filter_var($this->fromEmail, FILTER_VALIDATE_EMAIL)) {
-            $mail->from($this->fromEmail, (string) ($this->fromName ?? ''));
-        }
-
-        $replyTo = $this->replyToEmail ?: $this->fromEmail;
-        if ($replyTo && filter_var($replyTo, FILTER_VALIDATE_EMAIL)) {
-            $mail->replyTo($replyTo, (string) ($this->fromName ?? ''));
-        }
-
-        return $mail;
+        return $this->subject($this->emailSubject)
+                    ->html($this->htmlContent);
     }
 }
